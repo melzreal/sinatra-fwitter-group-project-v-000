ec3e8f648b82c8b2025fc49dfc3723c5b02482a4
Melissa Real <melissa.real@hotmail.com>
2018-12-11 16:18:15 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,40 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    if session[:user_id] == @user.id
+      erb :'tweets/tweets'
+    else
+      erb :'users/error'
+    end
+  end
+
+
+  get '/tweets/new' do
+    erb :'tweets/new'
+  end
+
+  get '/tweets/:id' do
+    @tweet = Tweet.find(params[:id])
+    erb :'tweets/show_tweet'
+  end
+
+  get '/tweets/:id/edit' do
+    @tweet = Tweet.find(params[:id])
+    erb :'tweets/edit_tweet'
+  end
+
+  post '/tweets' do
+    @tweet = Tweet.create(params['tweet'])
+    redirect to "/tweets/#{@tweet.id}"
+  end
+
+  patch '/tweets/:id' do
+    @tweet = Tweet.find(params[:id])
+    @tweet.update(params[:tweet])
+    @tweet.save
+    redirect to "/tweets/#{@tweet.id}"
+  end
+
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,45 @@
 class UsersController < ApplicationController
 
 
+  get '/signup' do
+    erb :'users/create_user'
+  end
+
+  post '/signup' do
+    if params[:user][:username].empty? || params[:user][:email].empty? || params[:user][:password].empty?
+      erb :'/users/error'
+     else
+       @user = User.create(params[:user])
+       session[:user_id] = @user.id
+       redirect '/login'
+     end
+
+  end
+
+  get '/login' do
+    erb :'/users/login'
+  end
+
+  post '/login' do
+    binding.pry
+    @user = User.find_by(username: params[:user][:username], password: params[:user][:password])
+    if !@user.nil?
+      session[:user_id] = @user.id
+      redirect '/user/home'
+    end
+     redirect '/tweets'
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/'
+  end
+
+  get '/users/home' do
+    @user = User.find(session[:user_id])
+    @tweets = Tweet.find_by(@user.id)
+    erb :'/users/show'
+  end
+
+
 end
