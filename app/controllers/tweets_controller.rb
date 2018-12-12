18cd4d660ffc0e82b76577750ee3e8f65d7a0bb0
Melissa Real <melissa.real@hotmail.com>
2018-12-12 19:47:42 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,78 @@
 class TweetsController < ApplicationController
 
 
+
+  get '/tweets' do
+    if session.has_key?(:user_id)
+      @user = User.find(session[:user_id])
+      @tweet = Tweet.find_by_id(:user_id)
+      erb :tweets
+    else
+      erb :'/login'
+    end
+  end
+
+  post '/tweets' do
+    if !params[:tweet].empty?
+      @user = User.find(session[:user_id])
+      @tweet = Tweet.create(params[:tweet])
+      @tweet.user_id = @user.id
+      @tweet.save
+      redirect to "/tweets/#{@tweet.id}"
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/tweets/new' do
+    if User.is_logged_in?(session)
+      @user = User.find(session[:user_id])
+      erb :'/tweets/new'
+    else
+      erb :'/users/error'
+    end
+  end
+
+  get '/tweets/:id' do
+    if User.is_logged_in?(session)
+      @tweet = Tweet.find(params[:id])
+      @user = User.find(session[:user_id])
+      erb :'/tweets/show_tweet'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    if User.is_logged_in?(session)
+    @tweet = Tweet.find(params[:id])
+    erb :'/tweets/edit_tweet'
+    else
+      redirect '/login'
+    end
+  end
+
+
+  patch '/tweets/:id' do
+      if User.is_logged_in?(session)
+        @tweet = Tweet.find(params[:id])
+        @tweet.update(params[:tweet]).save
+        redirect to '/tweets'
+     else
+        redirect '/login'
+     end
+  end
+
+  delete '/tweets/:id' do
+    if User.is_logged_in?(session)
+     @tweet = Tweet.find_by_id(params[:id])
+     @tweet.delete unless @tweet.user_id != session[:user_id]
+
+     redirect to '/tweets'
+    else
+     redirect to '/login'
+    end
+   end
+
+
 end
---
+++ app/models/user.rb
@@ -0,0 +1,30 @@
+class User < ActiveRecord::Base
+  has_many :tweets
+  validates_presence_of :username, :password
+
+
+  def slug
+    self.username.gsub(" ", "-").downcase
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find{ |stuff| stuff.slug == slug }
+  end
+
+  def self.is_logged_in?(session)
+      session.has_key?(:user_id)
+  end
+
+  def authenticate(pass)
+    @user = User.find_by(password: pass)
+    if !@user.nil?
+      @user
+    else
+      false
+    end
+  end
+
+
+
+
+end
