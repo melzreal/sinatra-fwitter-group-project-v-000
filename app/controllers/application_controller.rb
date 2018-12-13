8fb9a4a51becd5cdf2ea8c5424cfe685f1cfe4f0
Melissa Real <melissa.real@hotmail.com>
2018-12-13 10:50:59 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,16 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions
+    set :session_secret, "secret"
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
+  get '/' do
+    session.clear
+    erb :index
+  end
+
 
 end
